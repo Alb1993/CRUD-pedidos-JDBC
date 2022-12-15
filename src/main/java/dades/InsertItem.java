a740ede200a18b619728a6868b92e1c9df8c2bd1
trujillo.agut.carles <trujillo.agut.carles@alumnat.copernic.cat>
2022-12-15 11:00:16 +0100

    Añadido insert para productos // documentacion añadida a InsertItem

--- src/main/java/dades/InsertItem.java
+++ src/main/java/dades/InsertItem.java
@@ -1,10 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package dades;
 
 import aplicacio.model.Cliente;
+import aplicacio.model.Producto;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -13,14 +10,29 @@ import java.sql.Statement;
 /**
  *
  * @author Albert
+ * @version 1.0
+ *
+ * Clase para el Insert de datos en la BBDD
  */
 public class InsertItem {
 
+    // Insert de Clientes en la BBDD
     public void insertarCliente(Cliente cliente) throws SQLException {
         Connection con = DataSource.getConnection();
         Statement stmt = con.createStatement();
-        String query = "Insert into customers(customerEmail,idCard,customerName,phone,creditLimit,birthDate) values (" + cliente.getEmail() + "," + cliente.getDni() + "," + cliente.getNombre() + "," + cliente.getTelefono() + "," + cliente.getCreditoLimite() + "," + cliente.getFechaNacimiento() + ");";
+        String query = "Insert into customers(customerEmail,idCard,customerName,phone,creditLimit,birthDate) "
+                + "values (" + cliente.getEmail() + "," + cliente.getDni() + "," + cliente.getNombre() + ","
+                + cliente.getTelefono() + "," + cliente.getCreditoLimite() + "," + cliente.getFechaNacimiento() + ");";
         ResultSet rs = stmt.executeQuery(query);
     }
 
+    // Insert de Productos en la BBDD
+    public void insertarProducto(Producto producto) throws SQLException {
+        Connection con = DataSource.getConnection();
+        Statement stmt = con.createStatement();
+        String query = "Insert into products(productCode, productName, productDescription, quantityInStock, buyPrice) "
+                + "values (" + producto.getCode() + "," + producto.getNombre() + "," + producto.getDescripcion() + ","
+                + producto.getCantidadStock() + "," + producto.getPrecio() + ");";
+        ResultSet rs = stmt.executeQuery(query);
+    }
 }
