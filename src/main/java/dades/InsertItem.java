992ac01fe0205aab5d710acb8ee3ac26a533116a
=Albert Montero <=montero.villar.albert@alumnat.copernic.cat>
2022-12-15 10:17:08 +0100

    Insert Cliente realizado

--- src/main/java/dades/InsertItem.java
+++ src/main/java/dades/InsertItem.java
@@ -4,16 +4,23 @@
  */
 package dades;
 
+import aplicacio.model.Cliente;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 /**
  *
  * @author Albert
  */
 public class InsertItem {
-    
-    public static void InsertarCliente(){
-    
-    
-    
+
+    public void InsertarCliente(Cliente cliente) throws SQLException {
+        Connection con = DataSource.getConnection();
+        Statement stmt = con.createStatement();
+        String query = "Insert into customers(customerEmail,idCard,customerName,phone,creditLimit,birthDate) values (" + cliente.getEmail() + "," + cliente.getDni() + "," + cliente.getNombre() + "," + cliente.getTelefono() + "," + cliente.getCreditoLimite() + "," + cliente.getFechaNacimiento() + ");";
+        ResultSet rs = stmt.executeQuery(query);
     }
-    
+
 }
