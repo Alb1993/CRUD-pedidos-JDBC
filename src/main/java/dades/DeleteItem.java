2ccad11a1a9b965d1d5193773ac75c07b3ac163c
trujillo.agut.carles <trujillo.agut.carles@alumnat.copernic.cat>
2022-12-15 10:53:02 +0100

    Añadido delete para productos // documentacion añadida a DeleteItem

--- src/main/java/dades/DeleteItem.java
+++ src/main/java/dades/DeleteItem.java
@@ -1,10 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package dades;
 
 import aplicacio.model.Cliente;
+import aplicacio.model.Producto;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -13,13 +10,25 @@ import java.sql.Statement;
 /**
  *
  * @author Albert
+ * @version 1.0
+ * 
+ * Clase para borrar datos en la BBDD
  */
 public class DeleteItem {
    
-    public static void DeleteCliente(Cliente cliente) throws SQLException{
+    // Delete de clientes en la BBDD
+    public void deleteCliente(Cliente cliente) throws SQLException {
         Connection con = DataSource.getConnection();
         Statement stmt = con.createStatement();
         String query = "delete from customers where customerEmail = " + cliente.getEmail();
+        ResultSet rs = stmt.executeQuery(query);
+    }
+    
+    // Delete de productos en la BBDD
+    public void deleteProducto(Producto producto) throws SQLException {
+        Connection con = DataSource.getConnection();
+        Statement stmt = con.createStatement();
+        String query = "delete from products where productCode = " + producto.getCode();
         ResultSet rs = stmt.executeQuery(query);
     }
 
