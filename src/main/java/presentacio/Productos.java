ce16bd54284508bf1cd8b1379d6d83c344d5164e
trujillo.agut.carles <trujillo.agut.carles@alumnat.copernic.cat>
2022-12-15 12:40:34 +0100

    Diseño de pantalla de productos realizada

--- src/main/java/presentacio/Productos.java
+++ src/main/java/presentacio/Productos.java
@@ -1,13 +1,51 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package presentacio;
+
+import aplicacio.App;
+import java.io.IOException;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 
 /**
  *
- * @author Sergi
+ * @author D
+ * @version 1.0
+ * 
+ * Pantalla de productos
  */
+
 public class Productos {
-    
+
+    @FXML
+    private Button btn_add; // Añadir producto
+
+    @FXML
+    private Button btn_atras; // Volver a pantalla principal
+
+    @FXML
+    private Button btn_delete; // Borrar producto
+
+    @FXML
+    private Button btn_edit; // Editar producto
+
+    @FXML
+    void onClickAdd(ActionEvent event) throws IOException {
+
+    }
+
+    @FXML
+    void onClickAtras(ActionEvent event) throws IOException {
+        App.setRoot("principal");
+    }
+
+    @FXML
+    void onClickDelete(ActionEvent event) throws IOException {
+
+    }
+
+    @FXML
+    void onClickEdit(ActionEvent event) throws IOException {
+
+    }
+
 }
